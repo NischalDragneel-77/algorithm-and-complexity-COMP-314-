aba6195b82d43512a87fe62019a48890f1ce519f
DDragneel <nbhn333@gmail.com>
2022-03-19 22:22:04 +0545

    separate graph for each

--- insertion+merge sort/graphPlot.py
+++ insertion+merge sort/graphPlot.py
@@ -1,4 +1,4 @@
-import matplotlib.pyplot as plot
+import matplotlib.pyplot as plt
 import time
 import random
 
@@ -6,7 +6,6 @@ from numpy import average
 from insertionSort import insertion_sort
 from mergeSort import merge_sort
 
-fig, (plt1, plt2) = plot.subplots(nrows=1, ncols=2)
 
 randomList = range(10,1000,10)
 
@@ -32,50 +31,48 @@ def insertion_sort_plot():
         insertion_sort(C)
         avgCaseEndTime = time.time()
         avgCaseTime.append((avgCaseEndTime-avgCaseStartTime)*1000*1000)
-        print(f"arrayy with elements {len(A)} sorted in {bestCaseEndTime-bestCaseStartTime} seconds")
-        print(f"arrayy with elements {len(A)} sorted in {avgCaseEndTime-avgCaseStartTime} seconds")
-        print(f"arrayy with elements {len(A)} sorted in {worstCaseEndTime-worstCaseStartTime} seconds")
 
-    plt1.set_title("Insertion Sort")                                #title of the graph
-    plt1.set_xlabel("Array Size")                                   #x-axis label
-    plt1.set_ylabel("Time(in microseconds)")                        #y-axis label
-    plt1.plot(randomList, bestCaseTime, '*', label="Best Case")     #plotting the best case
-    plt1.plot(randomList, worstCaseTime, '+', label="Worst Case")   #plotting the worst case
-    plt1.plot(randomList, avgCaseTime, '.', label="Average Case")   #plotting the average case
-    plt1.legend()
+    plt.figure("Insertion Sort-Best Case")
+    plt.title("Insertion Sort")                                    #title of the graph
+    plt.xlabel("Array Size")                                       #x-axis label
+    plt.ylabel("Time(in microseconds)")                            #y-axis label
+    plt.plot(randomList, bestCaseTime, '*', label="Best Case")     #plotting the best case
+    plt.legend()
+    plt.figure("Insertion Sort-Worst Case")
+    plt.title("Insertion Sort")                                    #title of the graph
+    plt.xlabel("Array Size")                                       #x-axis label
+    plt.ylabel("Time(in microseconds)")  
+    plt.plot(randomList, worstCaseTime, '+', label="Worst Case")   #plotting the worst case
+    plt.legend()
+    plt.figure("Insertion Sort-Average Case")
+    plt.title("Insertion Sort")                                    #title of the graph
+    plt.xlabel("Array Size")                                       #x-axis label
+    plt.ylabel("Time(in microseconds)")  
+    plt.plot(randomList, avgCaseTime, '.', label="Average Case")   #plotting the average case
+    plt.legend()
 
 def merge_sort_plot():
     bestCaseTime = []
     worstCaseTime = []
-    avgCaseTime=[]                                                  
 
     for num in randomList:
         A = list(range(num))
-        B = list(reversed(A))
-        C = list(A)
+        B = list(A)
+        random.shuffle(B)
         bestCaseStartTime = time.time()
         merge_sort(A,0,len(A)-1)
         bestCaseEndTime = time.time()
         bestCaseTime.append((bestCaseEndTime-bestCaseStartTime)*1000*1000)
-        worstCaseStartTime = time.time()
-        merge_sort(B,0,len(B)-1)
-        worstCaseEndTime = time.time()
-        worstCaseTime.append((worstCaseEndTime-worstCaseStartTime)*1000*1000)
-        avgCaseStartTime = time.time()
-        merge_sort(C,0,len(C)-1)
-        avgCaseEndTime = time.time()
-        avgCaseTime.append((avgCaseEndTime-avgCaseStartTime)*1000*1000)
 
-    plt2.set_title("Merge Sort")
-    plt2.set_xlabel("Array Size")
-    plt2.set_ylabel("Time(in microseconds)")
-    plt2.plot(randomList, bestCaseTime, '*', label="Best Case")
-    plt2.plot(randomList, worstCaseTime, '+', label="Worst Case")
-    plt2.plot(randomList, avgCaseTime, '.', label="Average Case")   
-    plt2.legend()
+    plt.figure("Merge Sort-Best Case")
+    plt.title("Merge Sort")
+    plt.xlabel("Array Size")
+    plt.ylabel("Time(in microseconds)")
+    plt.plot(randomList, bestCaseTime, '*', label="Best Case")
+    plt.legend()
 
 
 if __name__ == '__main__':
     merge_sort_plot()
     insertion_sort_plot()
-    plot.show()
+    plt.show()
